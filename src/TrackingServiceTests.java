c609becf32fd7950298e5fddd498872907dfd422
John Lundgren <john.paul.lundgren@gmail.com>
2016-03-08 13:13:56 +0100

    Creation of ProteinTrackerTests

---
+++ src/TrackingServiceTests.java
@@ -0,0 +1,65 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class TrackingServiceTests {
+	
+	private TrackingService service;
+	
+	//Gets invoked before class, method must be static
+	@BeforeClass
+	public static void beforeClass(){
+		System.out.println("Before class");
+	}
+	
+	//Gets invoked after class, method must be static
+	@AfterClass
+	public static void afterClass(){
+		System.out.println("After class");
+	}
+	
+	//Gets invoked before every Test method
+	@Before
+	public void setUp(){
+		System.out.println("Before test method");
+		service = new TrackingService();
+	}
+	
+	//Gets invoked after every Test method
+	@After
+	public void tearDown(){
+		System.out.println("After test method");
+	}
+
+	@Test
+	public void newTrackingServiceTotalIsZero(){
+		assertEquals("Tracking service total was not zero", 0, service.getTotal());
+	}
+	
+	@Test
+	public void whenAddingProteinTotalIncreasesByThatAmount(){
+		service.addProtein(10);
+		assertEquals("Protein amount was not correct", 10, service.getTotal());
+	}
+	
+	@Test
+	public void whenRemovingProteinTotalRemainsZero(){
+		service.removeProtein(5);
+		assertEquals("Tracking service total was not zero", 0, service.getTotal());
+	}
+	
+	//The test method expects that an exception is going be thrown
+	@Test(expected = InvalidGoalException.class)
+	public void whenGoalIsSetToLessThanZeroExceptionIsThrown() throws InvalidGoalException{
+		service.setGoal(-5);
+	}
+	
+	//The test method must finish before the timeout constraint (in milliseconds)
+	@Test(timeout = 200)
+	@Ignore //Skips running this method
+	public void badTest(){
+		for (int i = 0; i < 10000000; i++) {
+			service.addProtein(1);
+		}
+	}
+}
